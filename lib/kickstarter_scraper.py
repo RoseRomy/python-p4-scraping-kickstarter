991fc0bc9d8f4dfd3fff3a45690a00c01f20b3c7
Rosemary Kamau <kamaurosemary.n@gmail.com>
2025-06-21 18:46:12 +0300

    Initial commit

--- lib/kickstarter_scraper.py
+++ lib/kickstarter_scraper.py
@@ -8,6 +8,7 @@ import ipdb
 # location: project.select("ul.project-meta span.location-name")[0].text
 # percent_funded: project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
 
+
 def create_project_dict():
     html = ''
     with open('./fixtures/kickstarter.html') as file:
@@ -18,14 +19,15 @@ def create_project_dict():
     for project in kickstarter.select("li.project.grid_4"):
         title = project.select("h2.bbcard_name strong a")[0].text
         projects[title] = {
-        'image_link': project.select("div.project-thumbnail a img")[0]["src"],
-        'description': project.select("p.bbcard_blurb")[0].text,
-        'location': project.select("ul.project-meta span.location-name")[0].text,
-        'percent_funded': project.select("ul.project-stats li.first.funded strong")[0].text.replace("%","")
+            'image_link': project.select("div.project-thumbnail a img")[0]["src"],
+            'description': project.select("p.bbcard_blurb")[0].text,
+            'location': project.select("ul.project-meta span.location-name")[0].text,
+            'percent_funded': project.select("ul.project-stats li.first.funded strong")[0].text.replace("%", "")
         }
     # return the projects dictionary
 
     return projects
+
 
 projects = create_project_dict()
 print(projects)
